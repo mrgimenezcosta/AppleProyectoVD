51b06f5935c0dcd52913f204c28fa2359d0a5a16
mrgimenezcosta <mgimenezcosta@mail.utdt.edu>
2024-06-23 17:19:37 -0300

    graficos

--- src/App.svelte
+++ src/App.svelte
@@ -2,7 +2,6 @@
   import Scroller from "@sveltejs/svelte-scroller";
   import DebugScroller from "./components/DebugScroller.svelte";
   import { onMount } from "svelte";
-  import { style } from "d3";
 
   let count;
   let index;
@@ -12,22 +11,25 @@
   let threshold = 0.5;
   let bottom = 0.9;
 
-  let charts = {
-    0: "barra_1980.png",
-    1: "lineas_stock_1984.png",
-    2: "lineas_stock_1984.png",
-    3: "lineas_stock_1984.png",
-    4: "lineas_stock_1998.png",
-    5: "lineas_income_1998.png",
-    6: "chart_2016.png"
-  };
+  let years = ["1980", "1984", "1985", "1997", "1998", "2001", "2001", "2007","2007", "2010", "2011", "2014", "2014", "2016", "2023", "2023"];
+  let iframes = [
+    "https://datawrapper.dwcdn.net/dF8Wa/1/",
+    "https://datawrapper.dwcdn.net/zsQMZ/5/",
+    "https://datawrapper.dwcdn.net/zsQMZ/5/",
+    "https://datawrapper.dwcdn.net/P0LxA/2/",
+    "https://datawrapper.dwcdn.net/hputn/3/",
+    "https://datawrapper.dwcdn.net/AT22n/1/",
+    "https://datawrapper.dwcdn.net/Q10dh/1/",
+    "https://datawrapper.dwcdn.net/17jj7/1/",
+    "https://datawrapper.dwcdn.net/2uwHx/1/", 
+    "https://datawrapper.dwcdn.net/dChO2/4/"
+  ];
 
-  let years = ["1980", "1984", "1985", "1997", "1998", "2001", "2006", "2007", "2010", "2011", "2014", "2014", "2016", "2023", "2023"];
   let icons = {
     1984: "icon_1984.png",
     1998: "icon_1998.png",
     2001: "icon_2001.png",
-    2006: "icon_2006.png",
+    // 2006: "icon_2006.png",
     2007: "icon_2007.png",
     2010: "icon_2010.png",
     2014: "icon_2014.png",
@@ -60,8 +62,7 @@
     <h3 class="headline">La evolución según Apple</h3>
     <p class="bajada">Think different.</p>
     <h3 class="anios">1976</h3>
-    <p class="texto">Apple, la empresa fundada por Steve Jobs, Steve Wozniak y Ronald Wayne, nacio en el garaje de los padres de Jobs en Cupertino, 1976. Su primer producto, conocido como el <b>Apple I</b>, consistía en un ordenador personal que combinaba un microprocesador con una conexión para un teclado y un ratón.</p>
-    <!-- <img src="/images/primer_anuncio.png" width="600" alt="primer publicidad de apple" /> -->
+    <p class="texto">Apple, la empresa fundada por Steve Jobs, Steve Wozniak y Ronald Wayne, nació en el garaje de los padres de Jobs en Cupertino, 1976. Su primer producto, conocido como el <b>Apple I</b>, consistía en un ordenador personal que combinaba un microprocesador con una conexión para un teclado y un ratón.</p>
   </div>
 
   {#if progress < 1}
@@ -88,9 +89,13 @@
     bind:offset={offset}
     bind:progress={progress}
   >
-  <div slot="background" class="image_container">
-    <img src="/images/{charts[index]}" height="700" alt="chart {index}" class="charts" />
-  </div>
+    <div slot="background" class="image_container">
+      {#if iframes[index]}
+        <iframe title="Ingresos por segmentos" aria-label="Interactive area chart" id="datawrapper-chart" src="{iframes[index]}" scrolling="no" frameborder="0" style="width: 100%; border: none;" height="700" data-external="1"></iframe>
+        <script type="text/javascript">!function(){"use strict";window.addEventListener("message",(function(a){if(void 0!==a.data["datawrapper-height"]){var e=document.querySelectorAll("iframe");for(var t in a.data["datawrapper-height"])for(var r=0;r<e.length;r++)if(e[r].contentWindow===a.source){var i=a.data["datawrapper-height"][t]+"px";e[r].style.height=i}}}))}();
+        </script>
+      {/if}
+    </div>
 
     <div slot="foreground" class="foreground_container">
       <section class="step_foreground">
@@ -103,23 +108,9 @@
           <p class="texto">Uno de los eventos que marcó el gran éxito de la compañía y que es uno de los principales hitos de la historia de Apple es la presentación del <b>Macintosh</b>, que se llevó a cabo el 24 de enero de 1984, de la mano de Steve Jobs. A raíz de esta presentación se empezó a ver un cambio, de cierta manera, radical en el mundo de la informática, ya que este dispositivo se considera el primer ordenador personal con interfaz gráfica.</p>
         </div>
       </section>
-      <!-- <section class="step_foreground_foto">
-        <div class="epi_foreground_foto">
-          <img src="/images/macintosh.png" width="600" alt="primer publicidad de apple" />
-        </div>
-      </section> -->
-      <!-- <section class="step_foreground_foto">
-        <div class="epi_foreground_foto">
-          <video width="800" style="margin-bottom: 80px;" controls>
-            <source src="/images/anuncioMacintosh.mp4" type="video/mp4">
-            <track kind="captions" src="/videos/subtitulos.vtt" srclang="es" label="Español">
-            Tu navegador no soporta la reproducción de videos.
-          </video>
-        </div>
-      </section> -->
       <section class="step_foreground">
         <div class="epi_foreground">
-          <p class="texto">Apple solicita la renuncia a Steve Jobs. Despues de esto, Apple continua pero se estanca y llegó a estar cerca de la quiebra.</p>
+          <p class="texto">Apple solicita la renuncia a Steve Jobs. Después de esto, Apple continúa pero se estanca y llegó a estar cerca de la quiebra.</p>
         </div>
       </section>
       <section class="step_foreground">
@@ -132,59 +123,53 @@
           <p class="texto">Con el regreso de Steve Jobs, Apple lanzó el <b>iMac G3</b>, una computadora todo en uno que destacaba por su diseño único y colorido. El iMac no solo fue un éxito comercial, revitalizando las finanzas de Apple, sino que también simbolizó el renacimiento de la marca Apple como sinónimo de innovación.</p>
         </div>
       </section>
-      <!-- <section class="step_foreground_foto">
-        <div class="epi_foreground_foto">
-          <img src="/images/ImacG3.png" width="600" alt="G3" />
-        </div>
-      </section> -->
       <section class="step_foreground">
         <div class="epi_foreground">
           <p class="texto">Apple presentó el <b>iPod</b>, un reproductor de audio digital. Un producto que revoluciona la industria de la música digital.</p>
         </div>
       </section>
-      <!-- <section class="step_foreground_foto">
-        <div class="epi_foreground_foto">
-          <img src="/images/Ipod1.png" width="300" alt="G3" />
-        </div>
-      </section> -->
       <section class="step_foreground">
+        <div class="epi_foreground">
+          <p class="texto">Entre 2003 y 2006, los precios de las acciones de Apple subieron de USD 6 a mas de USD 80 por acción. Un aumento porcentual de 1233.33%, esta subida tan grande se le puede atribuir a las ventas del iPod.</p>
+        </div>
+      </section>
+
+      <!-- <section class="step_foreground">
         <div class="epi_foreground">
           <p class="texto">Hasta 2006, Apple tenía en su catálogo varios modelos de iBook y PowerBook, que componían su línea de equipos portátiles. Sin embargo, ese año la compañía presentó una evolución de lo que ya existía, pero ahora con un estilo renovado y un nombre nuevo: <b>MacBook</b>.</p>
         </div>
-      </section>
+      </section> -->
+
       <section class="step_foreground">
         <div class="epi_foreground">
-          <p class="texto">Steve Jobs anunció que iba a presentar tres productos: “un iPod con gran pantalla táctil, un teléfono móvil revolucionario, y un dispositivo avanzado de comunicación conectado a Internet”. Esos tres productos eran uno solo: El <b>Iphone</b>.</p>
+          <p class="texto">El 9 de enero de 2007, Steve Jobs anunció que iba a presentar tres productos: “un iPod con gran pantalla táctil, un teléfono móvil revolucionario, y un dispositivo avanzado de comunicación conectado a Internet”. Esos tres productos eran uno solo: El <b>Iphone</b>.</p>
         </div>
       </section>
-      <!-- <section class="step_foreground_foto">
-        <div class="epi_foreground_foto">
-          <img src="/images/Iphone1.png" width="400" alt="G3" />
+      
+      <section class="step_foreground">
+        <div class="epi_foreground">
+          <p class="texto">Las acciones de Apple se dispararon a un máximo histórico de USD 97 por acción.</p>
         </div>
-      </section> -->
+      </section>
+
       <section class="step_foreground">
         <div class="epi_foreground">
           <p class="texto">Steve Jobs vuelve a sorprender al mundo con el anuncio de <b>iPad</b>.</p>
         </div>
       </section>
-      <!-- <section class="step_foreground_foto">
-        <div class="epi_foreground_foto">
-          <img src="/images/Ipad1.png" width="800" alt="G3" />
-        </div>
-      </section> -->
       <section class="step_foreground">
         <div class="epi_foreground">
-          <p class="texto">Este año marco el fallecimiento de Steve Jobs, pero también fue el año en el que Apple se convirtio en la empresa más valiosa del mundo. Tim Cook asume como CEO.</p>
+          <p class="texto">Este año marcó el fallecimiento de Steve Jobs, pero también fue el año en el que Apple se convirtió en la empresa más valiosa del mundo. Tim Cook asume como CEO.</p>
         </div>
       </section>
       <section class="step_foreground">
         <div class="epi_foreground">
-          <p class="texto">Tim Cook presenta el iphone 6 y 6 plus, que juntos se convertirían en el smartphone más vendido hasta ese momento, llegando a vender 247.5 millones modelos. </p>
+          <p class="texto">Tim Cook presenta el iphone 6 y 6 plus, que juntos se convertirían en el smartphone más vendido hasta ese momento, llegando a vender 247.5 millones de modelos. </p>
         </div>
       </section>
       <section class="step_foreground">
         <div class="epi_foreground">
-          <p class="texto"> En este mismo evento se anuncia el <b>Apple Watch</b>, que se convierte en el reloj, no solo smartwatch, más vendido hasta el momento.</p>
+          <p class="texto">En este mismo evento se anuncia el <b>Apple Watch</b>, que se convierte en el reloj, no solo smartwatch, más vendido hasta el momento.</p>
         </div>
       </section>
       <section class="step_foreground">
@@ -194,12 +179,12 @@
       </section>
       <section class="step_foreground">
         <div class="epi_foreground">
-          <p class="texto"> Anunciando la fecha de lanzamiento del <b>Vision Pro</b> Tim Cook explico: "Apple Vision Pro es el dispositivo de electrónica de consumo más avanzado jamás creado.Su revolucionaria y mágica interfaz de usuario redefinirá cómo nos conectamos, creamos y exploramos".</p>
+          <p class="texto">Anunciando la fecha de lanzamiento del <b>Vision Pro</b> Tim Cook explicó: "Apple Vision Pro es el dispositivo de electrónica de consumo más avanzado jamás creado. Su revolucionaria y mágica interfaz de usuario redefinirá cómo nos conectamos, creamos y exploramos".</p>
         </div>
       </section>
       <section class="step_foreground">
         <div class="epi_foreground">
-          <p class="texto">Las acciones de Apple aumentaron un 1,5% en las primeras operaciones del lunes tras el anuncio de la fecha de lanzamiento del Vision Pro. </p>
+          <p class="texto">Las acciones de Apple aumentaron un 1,5% en las primeras operaciones del lunes tras el anuncio de la fecha de lanzamiento del Vision Pro.</p>
         </div>
       </section>
     </div>
@@ -207,7 +192,6 @@
 </main>
 
 <style>
-
   .header {
     display: flex;
     justify-content: center;
@@ -216,13 +200,13 @@
     margin-top: 50px;
     margin-bottom: 80px;
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica', sans-serif;
+    text-align: center;
   }
 
   .headline {
     font-size: 100px;
     line-height: 1.2;
     font-weight: bold;
-    text-align: center;
     margin: 20px;
     color: #cecece;
   }
@@ -230,7 +214,6 @@
   .bajada {
     font-size: 35px;
     font-weight: bold;
-    text-align: center;
     margin: 10px;
     color: #acabab;
   }
@@ -238,21 +221,25 @@
   .anios {
     font-size: 40px;
     font-weight: bold;
-    text-align: center;
     color: #ffffff;
   }
 
-  /* Estilos para el scroller */
+  .texto {
+    font-size: 20px;
+    text-align: justify;
+    max-width: 740px;
+  }
+
   .foreground_container {
     pointer-events: none;
     display: flex;
     flex-direction: column;
     justify-content: flex-start;
     align-items: flex-end;
-    padding-right: 50px; /* Ajusta este valor según sea necesario */
+    padding-right: 50px;
   }
 
-  .step_foreground{
+  .step_foreground {
     display: flex;
     justify-content: center;
     align-items: center;
@@ -270,13 +257,6 @@
     justify-content: center;
     align-items: center;
     border-radius: 10px;
-  }
-
-  .texto {
-    margin-bottom: 50px;
-    max-width: 740px;
-    font-size: 20px;
-    justify-content: center;
   }
 
   .image_container {
@@ -301,7 +281,7 @@
   }
 
   .timeline.visible {
-    display: block; /* Mostrar cuando se cumplan las condiciones */
+    display: block;
   }
 
   .timeline-item {
@@ -311,25 +291,25 @@
   }
 
   .timeline-item img {
-    width: 70px; /* Ajustar el tamaño de los íconos */
+    width: 70px;
     margin-left: 10px;
   }
 
   .timeline-item .large-icon {
-    width: 100px; /* Ajusta el tamaño según sea necesario */
+    width: 100px;
     height: auto;
   }
 
   .year-text {
-    transition: font-size 0.3s ease, color 0.3s ease; /* Transición para tamaño y color */
+    transition: font-size 0.3s ease, color 0.3s ease;
   }
 
   .year-text.grey {
-    color: rgba(90, 90, 90, 0.767); /* Color gris para años no activos */
+    color: rgba(90, 90, 90, 0.767);
   }
 
   .year-text.active {
-    font-size: 50px; /* Tamaño de la letra cuando está activo */
-    color: rgb(255, 255, 255); /* Color del texto cuando está activo */
+    font-size: 50px;
+    color: rgb(255, 255, 255);
   }
 </style>
